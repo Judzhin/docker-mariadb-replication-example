706914018ec23dd86fe802a9165e99422d5a0e18
Judzhin Miles <info@msbios.com>
2021-03-23 16:02:29 +0200

    Intermediate commit

--- .docker/mariadb/data/slave.sql
+++
@@ -1,7 +0,0 @@
-CHANGE
-MASTER TO MASTER_HOST='mariadb-master', MASTER_USER='root.slave', MASTER_PASSWORD='root.password',
-# Указанные значения мы берем из настроек Мастера
-MASTER_LOG_FILE = 'mysql-bin.000007', MASTER_LOG_POS = 107;
-
-# После этого запускаем репликацию на Слейве:
-START SLAVE;
---
+++ data/slave.sql
@@ -0,0 +1,16 @@
+CHANGE MASTER TO
+    MASTER_HOST='mariadb-master',
+    MASTER_USER='root_slave',
+    MASTER_PASSWORD='root.password',
+    MASTER_PORT=3306,
+    # Указанные значения мы берем из настроек Мастера
+    MASTER_LOG_FILE='master_replication-bin.000003',
+    MASTER_LOG_POS=4,
+    MASTER_CONNECT_RETRY=10;
+
+CHANGE MASTER TO
+    MASTER_LOG_FILE='master_replication-bin.000003',
+    MASTER_LOG_POS=4;
+
+# После этого запускаем репликацию на Слейве:
+START SLAVE;
